2a7486b3844fbf055bc49bc4238619aabdf795be
Jason David <davidson4988@gmail.com>
2025-03-16 21:40:38 +0530

    JS Task

---
+++ TS/TS setup/intro.js
@@ -0,0 +1,10 @@
+"use strict";
+//TODO Type Annotation
+let a = 5;
+// const b:boolean = "WTF" //!Type 'string' is not assignable to type 'boolean'.
+let c = [1, 2, 3];
+//TODO any type
+let d = "Hello";
+console.log(d);
+d = 5;
+console.log(d);
